83f49764d98b33ab79d4bca1d1150d01db4c66c9
Jess Planck <jess@funroe.net>
2015-04-10 11:07:49 -0500

    Start testing profile post view.

--- nicholls-org-template.php
+++ nicholls-org-template.php
@@ -21,60 +21,22 @@ get_header();
 			<div class="nicholls-fs-photo">
 				<?php the_post_thumbnail('nicholls-fs-medium'); ?>
 			</div>
-			<div class="nicholls-fs-info">
-				<div class="nicholls-fs-name"><h2><?php the_title(); ?></h2></div>
-				<?php nicholls_fs_display_meta_item( '_nicholls_fs_employee_title' ); ?>
-				<?php nicholls_fs_display_meta_item( '_nicholls_fs_employee_dept' ); ?>
-				<?php nicholls_fs_display_meta_item( '_nicholls_fs_employee_email' ); ?>
-				<?php nicholls_fs_display_meta_item( '_nicholls_fs_phone' ); ?>
-				<?php nicholls_fs_display_meta_item( '_nicholls_fs_office' ); ?>
-			</div>
 
 			<div class="nicholls-fs-bio">
 				<?php the_content(); ?>
 			</div>
 						
-<?php
-$nicholls_fs_courses = get_post_meta( get_the_ID(), '_nicholls_fs_courses', true );
-?>
 
-<?php if (!empty( $nicholls_fs_courses ) ) : ?>
-				<div class="nicholls-fs-courses">
-				<h3>Courses Taught</h3>
-				<table class="table-defaults table-center table-lines-vertical table-cells-even-down-grey table-border-black">
-					<tr>
-						<th>Course</th> <th>Description</th> <th>Meeting Days</th> <th>Meeting Times</th>
-					</tr>
-<?php
-
-foreach ( $nicholls_fs_courses as $course => $course_info ) {
-	echo '<tr>';
-
-	echo '<td>' . $course_info['course_title'] . '</td>';
-	echo '<td>' . $course_info['description'] . '</td>';
-	echo '<td>' . $course_info['course_meeting_days'] . '</td>';
-	echo '<td>' . $course_info['course_meeting_times'] . '</td>';
-
-	echo '</tr>';
-}
-
-?>			
-					
-				</table>	
-				</div>
-<?php endif; ?>
-
-<?php nicholls_fs_email_form(); ?>
 <?php 
 // Deebug
-/*
+
 $custom_fields = get_post_custom();
 
 foreach ( $custom_fields as $field_key => $field_values ) {
 	//if(!isset($field_values[0])) continue;
 	echo $field_key . '=>' . $field_values[0] . '<br />';
 }
-*/			
+			
 ?>
 			
 			</div>
